0066f680ea1814aaa892445de9431e8b49921b6d
finoptimal-dev <developer@finoptimal.com>
2017-07-31 15:33:01 -0400

    Handled jpeg's

--- fo_qbo/mime_types.py
+++ fo_qbo/mime_types.py
@@ -5,6 +5,7 @@ What MIME type should is uploaded file?
 MIME_TYPES = {
     ".docx" : "application/vnd.ms-word",
     ".jpg"  : "image/jpg",
+    ".jpeg" : "image/jpg",
     ".pdf"  : "application/pdf",
     ".png"  : "image/png",
     ".pptx" : "application/vnd.ms-powerpoint",
