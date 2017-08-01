0d2c36e1e4c44522b0659f5a7a90fa1043417f8e
finoptimal-dev <developer@finoptimal.com>
2017-08-01 10:37:49 -0400

    Handling all acceptable file types now

--- fo_qbo/mime_types.py
+++ fo_qbo/mime_types.py
@@ -3,11 +3,21 @@ What MIME type should is uploaded file?
 """
 
 MIME_TYPES = {
+    ".ai"   : "application/postscript",
+    ".doc"  : "application/msword",
     ".docx" : "application/vnd.ms-word",
+    ".eps"  : "application/postscript",
+    ".gif"  : "image/gif",
     ".jpg"  : "image/jpg",
-    ".jpeg" : "image/jpg",
+    ".jpeg" : "image/jpeg",
+    ".ods"  : "application/vnd.oasis.opendocument.spreadsheet",
     ".pdf"  : "application/pdf",
     ".png"  : "image/png",
     ".pptx" : "application/vnd.ms-powerpoint",
+    ".rtf"  : "text/rtf",
+    ".tiff" : "image/tiff",
+    ".txt"  : "text/plain",
+    ".xls"  : "application/vnd.ms-excel",
     ".xlsx" : "application/vnd.ms-excel",
+    ".xml"  : "text/xml",
 }
