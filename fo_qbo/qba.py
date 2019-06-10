a8ec2e240512a30b53c7520587f2895fd4a68486
FinOptimal Developer <developer@finoptimal.com>
2019-06-09 22:22:35 -0400

    Suppress excess verbosity: #60452

--- fo_qbo/qba.py
+++ fo_qbo/qba.py
@@ -328,7 +328,8 @@ class QBAuth2():
         
         if self.refresh_token is None:
             if self.vb < 8:
-                print("Rerun with verbosity >= 8 to request access!")
+                if self.vb > 1:
+                    print("Rerun with verbosity >= 8 to request access!")
                 self._has_access = False
                 return self._has_access
             self.oob()
