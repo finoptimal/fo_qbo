8f7f1f21148ff71adaefa04032d237b02b219db3
Christopher Stamp <christopher@finoptimal.com>
2023-11-08 12:37:59 -0600

    Log qbs calls.

--- fo_qbo/qbs.py
+++ fo_qbo/qbs.py
@@ -319,7 +319,17 @@ class QBS(LoggedClass):
             data=data,
             **params
         )
-        
+
+        try:
+            api_logger.info(f"{response.__hash__()} - {response.status_code} {response.reason} - "
+                            f"{response.request.method.ljust(4)} {response.url} - {response.json()}")
+        except Exception as ex:
+            try:
+                api_logger.info(f"{response.__hash__()} - {response.status_code} {response.reason} - "
+                                f"{response.request.method.ljust(4)} {response.url} - None")
+            except:
+                pass
+
         self.last_response = response
 
         if not hasattr(self, "resps"):
